b55197bee51793ac6ef3583646a1c988a985924b
vpushkar-git <vpushkar548@gmail.com>
2025-12-20 14:46:20 +0530

    Add files via upload

---
+++ input.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class input
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value of a.");
+        int a=sc.nextInt();
+        System.out.println("Enter the value of b.");
+        int b=sc.nextInt();
+        int sum=a+b;
+        System.out.println("The sum is: "+sum);
+    }
+}
