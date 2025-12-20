4cd9de5c3b32cce036a6de8c635ad378c4ba66e8
vpushkar-git <vpushkar548@gmail.com>
2025-12-20 14:45:32 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,11 @@
+public class sum
+{
+    public static void main(String args[])
+    {
+        int a,b;
+        a=5;
+        b=10;
+        int sum=a+b;
+        System.out.println(sum);
+    }
+}
