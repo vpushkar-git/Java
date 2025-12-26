d1a25d914cb26989708d0e99422143ed2fb3f5c1
vpushkar-git <vpushkar548@gmail.com>
2025-12-26 13:17:49 +0530

    Add files via upload

---
+++ largestofthree.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class largestofthree 
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        int a=sc.nextInt();
+        System.out.println("Enter second number: ");
+        int b=sc.nextInt();
+        System.out.println("Enter third number: ");
+        int c=sc.nextInt();
+        if(a > b && a > c)
+        {
+            System.out.println(a+" is largest number.");
+        }
+        else if(b > a && b > c )
+        {
+            System.out.println(b+" is the largest number.");
+        }
+        else
+        {
+            System.out.println(c+" is the largest number.");
+        }
+    }    
+}
