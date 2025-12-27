639651ed13237707ce3b01cd42eae87701f65ab1
vpushkar-git <vpushkar548@gmail.com>
2025-12-27 14:26:53 +0530

    Add files via upload

---
+++ temperaturequestion.java
@@ -0,0 +1,15 @@
+public class temperaturequestion 
+{
+    public static void main(String args[])
+    {
+        double temp=103.5;
+        if(temp>100.0)
+        {
+            System.out.println("You have fever");
+        }
+        else
+        {
+            System.out.println("You don't have fever");
+        }
+    }
+}
