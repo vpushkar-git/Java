108745cea2581a4640d1a6261bfba4458f9f8eb5
vpushkar-git <vpushkar548@gmail.com>
2025-12-29 14:50:51 +0530

    Add files via upload

---
+++ series.java
@@ -0,0 +1,17 @@
+public class series 
+{
+    public static void main(String args[])
+    {
+        for(int i=1;i<=10;i++)
+        {
+            System.out.print(i+" ");
+        }
+        // using while loop
+        int i=1;
+        while(i < 11)
+        {
+            System.err.print(i+" ");
+            i++;
+        }
+    }    
+}
