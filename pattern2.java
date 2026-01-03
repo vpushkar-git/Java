81be7b0266ff0051a424103eb82b9938004545bb
vpushkar-git <vpushkar548@gmail.com>
2026-01-03 14:53:53 +0530

    Add files via upload

---
+++ pattern2.java
@@ -0,0 +1,14 @@
+public class pattern2 
+{
+    public static void main(String args[])
+    {
+        for(int i=0;i<4;i++)
+        {
+            for(int j=1;j<4-i+1;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }    
+}
