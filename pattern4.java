ffa9aef7556a77c0159086847922a2471d273476
vpushkar-git <vpushkar548@gmail.com>
2026-01-03 22:50:26 +0530

    Add files via upload

---
+++ pattern4.java
@@ -0,0 +1,16 @@
+public class pattern4 
+{
+    public static void main(String args[])
+    {
+        char ch='A';
+        for(int i=1;i<=4;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println("");
+        }
+    }    
+}
